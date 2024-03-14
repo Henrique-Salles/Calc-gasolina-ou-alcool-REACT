c98827634312ecd518c371f213e25a6a2219dc87
Henrique-Salles <henriquesalles1520@gmail.com>
2024-03-14 19:07:05 -0300

    style reset

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+import logoIMG from "./assets/logo.png";
+
+function App() {
+  return (
+    <div>
+      <h1>TESTE</h1>
+    </div>
+  );
+}
+
+export default App;
