e381bc846b1f09a45509c5bd5b3e3bca15ab90ad
Henrique-Salles <henriquesalles1520@gmail.com>
2024-03-15 15:49:35 -0300

    form

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,34 @@ import logoIMG from "./assets/logo.png";
 function App() {
   return (
     <div>
-      <h1>TESTE</h1>
+      <main className="container">
+        <img src={logoIMG} alt="logo da calculadora" />
+        <h1 className="title">Qual a melhor oção?</h1>
+
+        <form className="form">
+          <label>Álcool (preço por Litro):</label>
+          <input
+            className="input"
+            type="number"
+            placeholder="3,99"
+            min="1"
+            step="0.01"
+            required
+          />
+
+          <label>Gasolina (preço por Litro):</label>
+          <input
+            className="input"
+            type="number"
+            placeholder="5,91"
+            min="1"
+            step="0.01"
+            required
+          />
+
+          <input className="button" type="submit" value="Calcular" />
+        </form>
+      </main>
     </div>
   );
 }
